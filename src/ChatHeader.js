77f4dce27da26dde244b39113ce7b33b1c119568
Luis Raygadas-Lara <lraygada@pnw.edu>
2018-07-03 14:39:26 -0400

    use a stateless functional component

---
+++ src/ChatHeader.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const ChatHeader = () =>
+{
+    render()
+    {
+        return (
+            <header className="ChatHeader">
+                <div className="roomInfo">
+                <h2>#general</h2>
+                <p>Announcements and general chat</p>
+                </div>
+            </header>
+        )
+    }
+}
+
+export default ChatHeader
