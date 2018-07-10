d79c96cc4cd13d15e52c0d9d22b3e7d22ab226fe
Luis Raygadas-Lara <lraygada@pnw.edu>
2018-07-10 01:33:25 -0400

    add MessageList components

--- src/MessageList.js
+++ src/MessageList.js
@@ -4,7 +4,18 @@ import Message from './Message'
 
 const MessageList = (props) => {
   return (
-    <div className="MessageList">
+    <div 
+        className="MessageList"
+        style = {styles.messageList}
+    >
+        <div styles = {styles.roomAnnouncement}>
+          <h3 styles = {styles.h3}>
+            #general
+          </h3>
+          <p>This is the very beginning of the #general room.</p>
+        </div>
+
+
       {
         props.messages.map(msg => (
           <Message message={msg} key={msg.id} />
@@ -13,5 +24,27 @@ const MessageList = (props) => {
     </div>
   )
 }
+
+const styles = 
+{
+  messageList: 
+  {
+    backgroundColor: 'white',
+    flex: 1,
+    paddingBottom: '1rem',
+    overflowY: 'scroll',
+  },
+
+  roomAnnouncement: 
+  {
+    padding: '2rem 1rem',
+  },
+
+  h3:
+  {
+    fontSize: '1.5rem',
+  }
+}
+
 
 export default MessageList
